cf4f0c479e05f1f2a9c48c7a9ed9fb9afe12be23
FarrelAnshari <farrelmaulana.anshari@gmail.com>
2025-12-26 11:32:33 +0700

    Main

---
+++ src/main/java/org/example/library/Main.java
@@ -0,0 +1,13 @@
+package org.example.library;
+
+import org.example.library.util.DataManager;
+import org.example.library.ui.SplashScreen;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        DataManager.loadData();
+        SwingUtilities.invokeLater(SplashScreen::new);
+    }
+}
